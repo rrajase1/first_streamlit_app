b54325e958f8b888ecfb18296d032b0b3049f448
Ranjith Rajasekharan <104330970+rrajase1@users.noreply.github.com>
2022-04-24 20:36:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 streamlit.title('my streamlit title\'s')
 streamlit.text('i can type anything here')
 streamlit.text('as much as i like ')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
