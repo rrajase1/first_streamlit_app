9bb9a8233be98e3541d7f59c53ebe62a2ce45509
Ranjith Rajasekharan <104330970+rrajase1@users.noreply.github.com>
2022-04-27 10:54:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,13 @@ def get_fruityvice_data(selected_fruit):
     fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/"+selected_fruit)
     fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
-  
+def get_fruitloadList():
+    with my_snowCnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
 st.header('Fruityvice Application\'s')
 st.title("Get Fruit list")
-
+#snowflake connection
+my_snowCnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+mydata_rows=get_fruitloadList()
+streamlit.dataframe(mydata_rows)
