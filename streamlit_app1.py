2db9554eb01079c26cc5f2ff035646683840dd11
Ranjith Rajasekharan <104330970+rrajase1@users.noreply.github.com>
2022-04-27 10:46:01 -0400

    Create streamlit_app1.py

---
+++ streamlit_app1.py
@@ -0,0 +1,88 @@
+import streamlit
+#import streamlit as st
+import pandas
+#import pandas as pd
+import requests
+import snowflake.connector
+from urllib.error import URLError
+#defining functions
+def get_fruityvice_data(selected_fruit):
+    fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/"+selected_fruit)
+    fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+  
+streamlit.title('my streamlit title\'s')
+streamlit.text('i can type anything here')
+streamlit.text('as much as i like ')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list =my_fruit_list.set_index('Fruit')
+fruits_selected=streamlit.multiselect('Pick Your Fruits:',list(my_fruit_list.index),['Avocado','Grapes'])
+fruits_to_show=my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
+fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
+fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+#output to screen
+streamlit.dataframe(fruityvice_normalized)
+
+#Snowflake connection
+
+my_snowCnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur=my_snowCnx.cursor()
+my_cur.execute("select current_user(),current_account(),current_region()")
+mydata_row=my_cur.fetchone()
+streamlit.text("Snow Flake Data:")
+streamlit.text(mydata_row)
+
+## don't run anything after here.
+#streamlit.stop()
+streamlit.text("commencted lines")
+
+my_cur.execute("select * from fruit_load_list")
+mydata_row=my_cur.fetchone()
+streamlit.text("Fruit Load List - First one:")
+streamlit.text(mydata_row)
+streamlit.text("Dataframe:")
+streamlit.dataframe(mydata_row)
+
+my_cur.execute("select * from fruit_load_list")
+mydata_row=my_cur.fetchall()
+streamlit.text("Fruit Load List - All:")
+streamlit.text(mydata_row)
+streamlit.text("Dataframe:")
+streamlit.dataframe(mydata_row)
+
+#Adding textbox
+streamlit.header("Pick yor Fruit")
+user_text=streamlit.text_input("Pick yor Fruit")
+streamlit.write("Thanks for adding:",user_text)
+
+#my_cur.execute("insert into fruit_load_list values('from streamlit')")
+
+## new section to display fruitywise api advice
+streamlit.header('Fruitywise fruit advice:')
+#user_text=streamlit.text_input("Fruit to get advice",'kiwi')
+try:
+  user_text=streamlit.text_input("Fruit to get advice")
+  if not user_text:
+      streamlit.error('Please select a fruit')
+  else:
+    # non function method start
+    #fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/"+user_text)
+    #fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+    #streamlit.dataframe(fruityvice_normalized)
+    # non function method end
+    
+    #function method start
+      backfrom_function=get_fruityvice_data(user_text)
+      streamlit.dataframe(backfrom_function)
+    #function method end
+except URLError as e:
+  streamlit.error()
