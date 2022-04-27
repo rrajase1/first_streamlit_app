bf3c624e3a2cce2178da203e66b7ed0204ee0acd
Ranjith Rajasekharan <104330970+rrajase1@users.noreply.github.com>
2022-04-27 11:34:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,4 @@ st.header("Pick yor Fruit")
 user_text=st.text_input("Pick yor Fruit")
 if not user_text:
       streamlit.error('Please select a fruit')
-  else:
-    backfrom_function=ins_dataSnowFlake(user_text)
-    st.dataframe(backfrom_function)
+
